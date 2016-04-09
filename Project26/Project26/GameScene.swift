8b74c4e7ca16ed529bae2860b3431c88c9d7c790
Dean Kuhta <dean@iteration15.com>
2016-04-09 18:23:51 -0400

    added createPlayer method

--- Project26/Project26/GameScene.swift
+++ Project26/Project26/GameScene.swift
@@ -17,7 +17,15 @@ enum CollisionTypes: UInt32 {
 }
 
 class GameScene: SKScene {
+    
+    var player: SKSpriteNode!
+    
     override func didMoveToView(view: SKView) {
+        let background = SKSpriteNode(imageNamed: "background.jpg")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .Replace
+        background.zPosition = -1
+        addChild(background)
         
         loadLevel()
         
@@ -53,9 +61,27 @@ class GameScene: SKScene {
                             node.physicsBody!.collisionBitMask = 0
                             addChild(node)
                         } else if letter == "s" {
-                            //load star
+                            let node = SKSpriteNode(imageNamed: "star")
+                            node.name = "star"
+                            node.physicsBody = SKPhysicsBody(circleOfRadius: node.size.width / 2)
+                            node.physicsBody!.dynamic = false
+                            
+                            node.physicsBody!.categoryBitMask = CollisionTypes.Star.rawValue
+                            node.physicsBody!.contactTestBitMask = CollisionTypes.Player.rawValue
+                            node.physicsBody!.collisionBitMask = 0
+                            node.position = position
+                            addChild(node)
                         } else if letter == "f" {
-                            //load finish
+                            let node = SKSpriteNode(imageNamed: "finish")
+                            node.name = "finish"
+                            node.physicsBody = SKPhysicsBody(circleOfRadius: node.size.width / 2)
+                            node.physicsBody!.dynamic = false
+                            
+                            node.physicsBody!.categoryBitMask = CollisionTypes.Finish.rawValue
+                            node.physicsBody!.contactTestBitMask = CollisionTypes.Player.rawValue
+                            node.physicsBody!.collisionBitMask = 0
+                            node.position = position
+                            addChild(node)
                         }
                     }
                 }
@@ -63,6 +89,18 @@ class GameScene: SKScene {
         }
     }
 
+    func createPlayer() {
+        player = SKSpriteNode(imageNamed: "player")
+        player.position = CGPoint(x: 96, y: 672)
+        player.physicsBody = SKPhysicsBody(circleOfRadius: player.size.width / 2)
+        player.physicsBody!.allowsRotation = false
+        player.physicsBody!.linearDamping = 0.5
+        
+        player.physicsBody!.categoryBitMask = CollisionTypes.Player.rawValue
+        player.physicsBody!.contactTestBitMask = CollisionTypes.Star.rawValue | CollisionTypes.Vortex.rawValue | CollisionTypes.Finish.rawValue
+        player.physicsBody!.collisionBitMask = CollisionTypes.Wall.rawValue
+        addChild(player)
+    }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        
