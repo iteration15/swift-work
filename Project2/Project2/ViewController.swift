e4f60fc4712cf60343c72a2d165b33944c06c7e0
Dean Kuhta <dean@iteration15.com>
2016-04-09 14:51:36 -0400

    added score logic

--- Project2/Project2/ViewController.swift
+++ Project2/Project2/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var button3: UIButton!
     
     var countries = [String]()
+    var correctAnswer = 0
     var score = 0
 
     override func viewDidLoad() {
@@ -42,17 +43,38 @@ class ViewController: UIViewController {
         button2.layer.borderColor = UIColor.lightGrayColor().CGColor
         button3.layer.borderColor = UIColor.lightGrayColor().CGColor
         
-        askQuestion()
+        askQuestion(nil)
     }
 
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
         
         countries = GKRandomSource.sharedRandom().arrayByShufflingObjectsInArray(countries) as! [String]
         
         button1.setImage(UIImage(named: countries[0]), forState: .Normal)
         button2.setImage(UIImage(named: countries[1]), forState: .Normal)
         button3.setImage(UIImage(named: countries[2]), forState: .Normal)
+        
+        correctAnswer = GKRandomSource.sharedRandom().nextIntWithUpperBound(3)
+        
+        title = countries[correctAnswer].uppercaseString
     }
+    
+    @IBAction func buttonTapped(sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .Alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .Default, handler: askQuestion))
+        presentViewController(ac, animated: true, completion: nil)
+    }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
