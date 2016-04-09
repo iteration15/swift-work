0ed882d494f8216704d5d51236219f46f5278b74
Dean Kuhta <dean@iteration15.com>
2016-04-09 17:55:58 -0400

    added enum and loadLevel method

--- Project26/Project26/GameScene.swift
+++ Project26/Project26/GameScene.swift
@@ -8,38 +8,54 @@
 
 import SpriteKit
 
+enum CollisionTypes: UInt32 {
+    case Player = 1
+    case Wall = 2
+    case Star = 4
+    case Vortex = 8
+    case Finish = 16
+}
+
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
-        
-        self.addChild(myLabel)
+       
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
-        
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
+       
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
-}
+    
+    func loadLevel() {
+        if let levelPath = NSBundle.mainBundle().pathForResource("level1", ofType: "txt") {
+            if let levelString = try? String(contentsOfFile: levelPath, usedEncoding: nil) {
+                let lines = levelString.componentsSeparatedByString("\n")
+                
+                for (row, line) in lines.reverse().enumerate() {
+                    for (column, letter) in line.characters.enumerate() {
+                        let position = CGPoint(x: (64 * column) + 32, y: (64 * row) + 32)
+                        
+                        if letter == "x" {
+                            let node = SKSpriteNode(imageNamed: "block")
+                            node.position = position
+                            
+                            node.physicsBody = SKPhysicsBody(rectangleOfSize: node.size)
+                            node.physicsBody!.categoryBitMask = CollisionTypes.Wall.rawValue
+                            node.physicsBody!.dynamic = false
+                            addChild(node)
+                        } else if letter == "v" {
+                            //load vortex
+                        } else if letter == "s" {
+                            //load star
+                        } else if letter == "f" {
+                            //load finish
+                        }
+                    }
+                }
+            }
+        }
+    }
+ }
