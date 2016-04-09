d4f47fddebb682a505adc1416f535c5da1e9be17
Dean Kuhta <dean@iteration15.com>
2016-04-09 16:39:47 -0400

    added names to target and ball

--- Project11/Project11/GameScene.swift
+++ Project11/Project11/GameScene.swift
@@ -32,6 +32,7 @@ class GameScene: SKScene {
             ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
             ball.physicsBody!.restitution = 0.4
             ball.position = location
+            ball.name = "ball"
             addChild(ball)
         }
        
@@ -52,13 +53,18 @@ class GameScene: SKScene {
         if isGood {
             slotBase = SKSpriteNode(imageNamed: "slotBaseGood")
             slotGlow = SKSpriteNode(imageNamed: "slotGlowGood")
+            slotBase.name = "good"
         } else {
             slotBase = SKSpriteNode(imageNamed: "slotBaseBad")
             slotGlow = SKSpriteNode(imageNamed: "slotGlowBad")
+            slotBase.name = "bad"
         }
         
         slotBase.position = position
         slotGlow.position = position
+        
+        slotBase.physicsBody = SKPhysicsBody(rectangleOfSize: slotBase.size)
+        slotBase.physicsBody!.dynamic = false
         
         addChild(slotBase)
         addChild(slotGlow)
