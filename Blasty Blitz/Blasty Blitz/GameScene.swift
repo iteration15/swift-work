cf366f82032cae2ba087066d8331ea4b82d5b494
Dean Kuhta <dean@iteration15.com>
2016-04-10 16:16:45 -0400

    added player

--- Blasty Blitz/Blasty Blitz/GameScene.swift
+++ Blasty Blitz/Blasty Blitz/GameScene.swift
@@ -9,37 +9,75 @@
 import SpriteKit
 
 class GameScene: SKScene {
-    override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
-        
-        self.addChild(myLabel)
+    
+    // Layered Nodes
+    var backgroundNode: SKNode!
+    var midgroundNode: SKNode!
+    var foregroundNode: SKNode!
+    var hudNode: SKNode!
+    var player: SKNode!
+    
+    // To Accommodate iPhone 6
+    var scaleFactor: CGFloat!
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
     }
     
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
+    override init(size: CGSize) {
+        super.init(size: size)
+        backgroundColor = SKColor.whiteColor()
         
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+        scaleFactor = self.size.width / 320.0
+        
+        // Create the game nodes
+        // Background
+        backgroundNode = createBackgroundNode()
+        addChild(backgroundNode)
+        
+        // Foreground
+        foregroundNode = SKNode()
+        addChild(foregroundNode)
+        
+        // Add the player
+        player = createPlayer()
+        foregroundNode.addChild(player)
+    }
+    
+    func createPlayer() -> SKNode {
+        let playerNode = SKNode()
+        playerNode.position = CGPoint(x: self.size.width / 2, y: 80.0)
+        
+        let sprite = SKSpriteNode(imageNamed: "Player")
+        playerNode.addChild(sprite)
+        
+        return playerNode
+    }
+    
+    func createBackgroundNode() -> SKNode {
+        // 1
+        // Create the node
+        let backgroundNode = SKNode()
+        let ySpacing = 64.0 * scaleFactor
+        
+        // 2
+        // Go through images until the entire background is built
+        for index in 0...19 {
+            // 3
+            let node = SKSpriteNode(imageNamed:String(format: "Background%02d", index + 1))
+            // 4
+            node.setScale(scaleFactor)
+            node.anchorPoint = CGPoint(x: 0.5, y: 0.0)
+            node.position = CGPoint(x: self.size.width / 2, y: ySpacing * CGFloat(index))
+            //5
+            backgroundNode.addChild(node)
         }
+        
+        // 6
+        // Return the completed background node
+        return backgroundNode
     }
-   
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
-    }
+
+
+
 }
